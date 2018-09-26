e698b30dd3c4db63f64378a94c4376e4a89490e5
Pham Hong Thai <leopham@Phams-Mac-mini.local>
2018-09-26 14:25:37 +0700

    use title service to set title page

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'leo-user',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  pageTile = 'Font end page';
+  constructor(private _title: Title) {}
 
   ngOnInit() {
+    this._title.setTitle(this.pageTile)
   }
-
 }
