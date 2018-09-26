2fc66de8112634fb12b00ba1376133e91cd181fa
Pham Hong Thai <leopham@Phams-Mac-mini.local>
2018-09-26 14:20:27 +0700

    split admin vs user section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
+import { UserModule } from './user/user.module';
+import { AdminModule } from './admin/admin.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: './user/user.module#UserModule' },
+  { path: 'admin', loadChildren: './admin/admin.module#AdminModule' },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
